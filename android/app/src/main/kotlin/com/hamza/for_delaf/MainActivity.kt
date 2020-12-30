27f0513b66bf3862585f4d1669376448fd3ee060
Hamza Hawkins <hamza@codeintelx.com>
2020-12-29 19:16:53 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/hamza/for_delaf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hamza.for_delaf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Text('I Will Be TikTok Famous'),
+          ),
+        body: Center(
+          child: Image(
+            image:
+              AssetImage('images/gifs/delaftiktok.gif'),
+          ),
+        )
+      )
+    )
+  );
+}
